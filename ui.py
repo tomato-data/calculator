3536abc57c3b67bb85d5cf099b124052fbe70447
tomato <ahsdfg30@gmail.com>
2024-11-17 16:49:04 +0900

    Revert "Merge origin/master"
    This reverts commit cbc4ae98d7ddc5d4a5f6c61518900e743f378695.

--- ui.py
+++ ui.py
@@ -13,13 +13,11 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
-        self.btn1 = QPushButton('Calc', self)
+        self.btn1 = QPushButton('Message', self)
         self.btn2 = QPushButton('Clear', self)
 
         self.le1 = QLineEdit('0', self)
         self.le1.setAlignment(QtCore.Qt.AlignRight)
-        self.le1.setFocus(True)
-        self.le1.selectAll()
 
         self.le2 = QLineEdit('0', self)
         self.le2.setAlignment(QtCore.Qt.AlignRight)
@@ -50,7 +48,7 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
 
-    def setDisplay(self, text):
+    def activateMessage(self, text):
         self.te1.appendPlainText(text)
 
     def clearMessage(self):
