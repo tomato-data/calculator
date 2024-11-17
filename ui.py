3520a9300bdc7169612df2332696070b7a3bc509
tomato <ahsdfg30@gmail.com>
2024-11-17 16:49:11 +0900

    Revert "Modify ui.py to add le1, le2 and cb widgets"
    This reverts commit ac2e4922239f3908bd0a625e9ca71e203eb494d1.

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # application handler, empty GUI widget
+                             QMessageBox, QPlainTextEdit, QHBoxLayout) # application handler, empty GUI widget
 from PyQt5.QtGui import QIcon
-from PyQt5 import QtCore
 
 class View(QWidget):
 
@@ -16,20 +15,6 @@ class View(QWidget):
         self.btn1 = QPushButton('Message', self)
         self.btn2 = QPushButton('Clear', self)
 
-        self.le1 = QLineEdit('0', self)
-        self.le1.setAlignment(QtCore.Qt.AlignRight)
-
-        self.le2 = QLineEdit('0', self)
-        self.le2.setAlignment(QtCore.Qt.AlignRight)
-
-        self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/'])
-
-        hbox_formular = QHBoxLayout()
-        hbox_formular.addWidget(self.le1)
-        hbox_formular.addWidget(self.cb)
-        hbox_formular.addWidget(self.le2)
-
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
@@ -37,7 +22,6 @@ class View(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addLayout(hbox_formular)
         vbox.addLayout(hbox)
         vbox.addStretch(1)
 
